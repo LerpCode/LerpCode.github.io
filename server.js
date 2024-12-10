639fba543be1172110dea8c2009a9a0a244051c3
Nguyen-The-Lap <lapnguyen10a5@gmail.com>
2024-12-10 12:56:20 +0700

    fixing

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path'); 
 
 const hostname = '127.0.0.1'; 
-const port = 3000; 
+const port = process.env.PORT || 8080; 
 
 const server = http.createServer((req, res) => { 
     let filePath = '.' + req.url; 
